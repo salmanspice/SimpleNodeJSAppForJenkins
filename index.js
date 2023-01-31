bf986db4ff2664b9732b1cf512302ee17d8f3110
Alan <alanohcz94@gmail.com>
2023-01-31 22:40:58 +0800

    Update code

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req,res) => {
+  res.send("Do or do not there is no try");
+});
+
+const server = app.listen(3000, ()=>{
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('Example application is currently listening on port 3000 and at http://%s:%s', host, port);
+})
