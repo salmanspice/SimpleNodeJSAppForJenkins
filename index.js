4acb4ad47af862e93fe7e051de61a8542578f39b
salmanspice <43702598+salmanspice@users.noreply.github.com>
2024-02-14 02:04:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require('node:http');
 const hostname = '127.0.0.1';
-const port = 3001;
+const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World..This is Jenkins Testing\n');
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
